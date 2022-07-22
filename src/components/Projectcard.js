7c0c6fe6322b253eadc907d7ddc8c5a19a448d48
caleb-ali <slimkally@gmail.com>
2022-07-22 02:12:18 +0100

    started projects section

---
+++ src/components/Projectcard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Projectcard.css";
+
+const Projectcard = () => {
+    return (
+        <div className="p">
+            Projectcard
+        </div>
+    )
+}
+
+export default Projectcard
