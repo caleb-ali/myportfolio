7fb3843600abb1f1354f164a7b6c3e95300b0ab9
caleb-ali <slimkally@gmail.com>
2022-07-28 13:38:16 +0100

    started project section

--- src/components/Projectlist.js
+++ src/components/Projectlist.js
@@ -7,6 +7,14 @@ import Projectcard from "./Projectcard";
 const Projectlist = () => {
   return (
     <div className="pl">
+
+      <h1>
+        My Recent Works
+      </h1>
+
+      <h3>
+        Here are a list of projects i have worked on in the past.
+      </h3>
       <div className="pl-list">
         <Projectcard />
         <Projectcard />
