e9f12aa6ff8a989bae3e42a7963e0e4a6175066e
caleb-ali <slimkally@gmail.com>
2022-08-11 02:31:43 +0100

    added hover property to cards

--- src/components/Projectcard.js
+++ src/components/Projectcard.js
@@ -7,20 +7,9 @@ import { Projectcards } from "./Data";
 const Projectcard = ({ img, link, title, technology, description }) => {
     return (
         <div className="p">
-            <a href={link} target="_blank" rel="nonreferrer">
-                <img src={img} alt="" className="p-img" />
-            </a>
-            <h4>
-                {title} 
-            </h4>
-            <h4>
-                {technology}    
-            </h4>
-            <h4>
-            {description}
-            </h4>
-           
-
+            <h2 className="card-title">{title}</h2>
+            <p className="tech">{technology}</p>
+            <p className="description">{description}</p>
         </div>
 
     )
--- src/components/Projectlist.js
+++ src/components/Projectlist.js
@@ -12,13 +12,10 @@ const Projectlist = () => {
       <h1>
         My Recent Works
       </h1>
-
-      <h3>
-        Here are a list of projects i have worked on in the past.
-      </h3>
+    
       <div className="pl-list">
         {Projectcards.map((item) => (
-          <Projectcard key={item?.id} img={item?.img} link={item?.link} title={item?.title} technology={item?.technology}  description={item?.description} />
+          <Projectcard key={item?.id} title={item?.title} technology={item?.technology}  description={item?.description} />
         ))}
     
       </div>
