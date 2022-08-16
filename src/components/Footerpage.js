f17e464dd441e662aeda31cd0207c82352d21ea0
caleb-ali <slimkally@gmail.com>
2022-08-17 00:10:10 +0100

    more changes

--- src/components/Footerpage.js
+++ src/components/Footerpage.js
@@ -10,8 +10,8 @@ import { BsMedium } from 'react-icons/bs';
 const Footerpage = () => {
   return (
     <div class="site-footer">
-      <hr></hr>
-      <p class="copyright-text">CALEB ALI &copy; 2022 </p>
+    
+      <p class="copyright-text">caleb ali &copy; 2022 </p>
       <div class="footer-links">
         <a href="https://twitter.com/_realkally" target="_blank" rel="noreferrer">  <BsTwitter />  </a>
         <a href="https://www.linkedin.com/in/caleb-ali-94305b14b/" target="_blank" rel="noreferrer">   <FaLinkedin /> </a>
