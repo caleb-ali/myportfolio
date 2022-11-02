b957e2246ae28ee7bc1b622fbd8c6fe34c4fd5de
caleb-ali <slimkally@gmail.com>
2022-11-02 18:11:45 +0100

    added under construction

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React from "react";
 import "./HeroimgStyles.css";
-import Introimg from "../assets/programmer.svg"
-import{HiDownload} from 'react-icons/hi';
-
+import Introimg from "../assets/programmer.svg";
+import { HiDownload } from "react-icons/hi";
+import { AiOutlineWarning } from "react-icons/ai";
 
 const Heroimg = () => {
   return (
     <div className="hero">
-        <div className='mask'>
-            <img className='intro-img' src={Introimg}
-             alt="Introimg"/>
+      <div className="mask">
+        <img className="intro-img" src={Introimg} alt="Introimg" />
+      </div>
+      <div className="content">
+        <h1>Hi. I'm Caleb</h1>
+        <h2> A Frontend developer</h2>
+        <button className="btn">
+          resume <HiDownload />{" "}
+        </button>
+        <div>
+          <h3>
+            <AiOutlineWarning /> under construction!!!
+          </h3>
         </div>
-        <div className='content'>
-            <h1>Hi. I'm Caleb</h1>
-            <h2> A Frontend developer</h2>
-            <button className='btn'>resume <HiDownload/> </button>
-        </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Heroimg;
-
-
